fc8bc1b2bc8542623e932ae5e43f5a39d4e1150f
Alexandru Rebenciuc <alrebenciuc@gmail.com>
2022-03-24 01:22:25 +0200

    finished app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create({ name: 'Mcdo', address: '12 Rue Test', phone_number: '05 46 47 51 62', category: 'chinese' })
+Restaurant.create({ name: 'Burger', address: '2 Rue Troi', phone_number: '04 85 62 56 85', category: 'italian' })
+Restaurant.create({ name: 'Philo', address: '5 Rue Mer', phone_number: '05 45 45 65 25', category: 'french' })
